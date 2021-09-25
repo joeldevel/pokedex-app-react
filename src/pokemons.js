905aad966220eaf07ed528205656e2ceb224f013
joeldevel <christianjoel.developer@gmail.com>
2021-09-24 22:30:35 -0300

    fix pokemons id

--- src/pokemons.js
+++ src/pokemons.js
@@ -5,8 +5,8 @@ const pokemonsData = [
 	{id: 12, name: 'ButterFree', type: 'flying', base_experience: 178},
 	{id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
 	{id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-	{id: 95, name: 'Gengar', type: 'poison', base_experience: 225},
-	{id: 127, name: 'Eevee', type: 'normal', base_experience: 65}
+	{id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+	{id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
 ];
 
 export default pokemonsData;
