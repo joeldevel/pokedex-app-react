542f36461f5d3f86af6e3f3047f3eafd728d82a6
joeldevel <christianjoel.developer@gmail.com>
2021-09-24 22:01:57 -0300

    Pokedex contains PokemonCards

---
+++ src/Pokedex.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PokemonCard from './PokemonCard';
+
+const Pokedex = () => {
+	return (
+		<div className="">
+	      	<PokemonCard/>
+	      	<PokemonCard/>
+	      	<PokemonCard/>
+	      	<PokemonCard/>
+		</div>
+	);
+};
+
+export default Pokedex;
