8850f17a12b981f3114fd56c4283ce069f4978d4
joeldevel <christianjoel.developer@gmail.com>
2021-09-24 22:22:36 -0300

    Pokedex holds some pokemons data(state)

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PokemonCard from './PokemonCard';
+import pokemonsData from './pokemons';
 
 const Pokedex = () => {
+	const [pokemons, setPokemons] = useState(null);
+
+	useEffect(()=>{
+		setPokemons(pokemonsData);
+	},[]);
+
 	return (
 		<div className="">
+			<h1>{pokemons ? pokemons[0].name: 'no pokemons'}</h1>
 	      	<PokemonCard/>
 	      	<PokemonCard/>
 	      	<PokemonCard/>
